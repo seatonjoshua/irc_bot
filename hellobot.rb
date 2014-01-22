c92dfef5f7f9f11b2c9f2adbfdc275f5427adf98
Joshua Seaton <joshuaseaton@Joshua-Seatons-MacBook-Pro.local>
2014-01-22 16:00:32 -0500

    add initial step joke bot

--- hellobot.rb
+++ hellobot.rb
@@ -4,30 +4,39 @@ server = "irc.bitmakerlabs.com"
 port = "6667"
 nick = "HelloBot2"
 channel = "#cohort5"
-greeting_prefix = "privmsg #bitmaker :"
-#greetings = ["hello", "hi", "hola", "yo", "wazup", "guten tag", "howdy", "salutations", "who the hell are you?"]
-greetings=["ziggy"]
+greeting_prefix = "privmsg #{channel} :"
+greetings=["who's there?", "there", "who's", "?"]
+greetings1=["dismay who?"]
 
 irc_server = TCPSocket.open(server, port)
 
 irc_server.puts "USER bhellobot2 0 * BHelloBot2"
 irc_server.puts "NICK #{nick}"
 irc_server.puts "JOIN #{channel}"
-irc_server.puts "PRIVMSG #{channel} :Hello from IRC Bot"
+irc_server.puts "PRIVMSG #{channel} :Knock, Knock!"
 
 # Hello, Bot!
 until irc_server.eof? do
   msg = irc_server.gets.downcase
   puts msg
 
-  was_greeted = false
+  stage_one = false
   greetings.each do |g|
-    was_greeted = true if msg.include?(g)
-    puts "********************  greeted: #{was_greeted}"
+    stage_one = true if msg.include?(g)
+    puts "greeted: #{stage_one}"
   end
 
-  if msg.include?(greeting_prefix) and was_greeted
-          response = "w00t! Someone talked to us!!!! Hello!!!"
-          irc_server.puts "PRIVMSG #{channel} :#{response}"
+  stage_two = false
+  greetings1.each do |g|
+    stage_two = true if msg.include?(g)
+    puts "greeted: #{stage_two}"
+  end
+
+  if msg.include?(greeting_prefix) && stage_one 
+    response = "dismay"
+    irc_server.puts "PRIVMSG #{channel} :#{response}"
+  elsif msg.include?(greeting_prefix) && stage_two 
+    response = "Dismay be a bad joke but I think it's funny!"
+    irc_server.puts "PRIVMSG #{channel} :#{response}"      
   end
 end
